2348f20db91fe45f1a0055377f0bb2354e6b5261
Farid Zellipour <82771409+FaridZelli@users.noreply.github.com>
2024-08-22 01:15:47 +0000

    Update tumbleweed_rocm_radeon.sh

--- tumbleweed_rocm_radeon.sh
+++ tumbleweed_rocm_radeon.sh
@@ -2,6 +2,7 @@
 
 # Script by Farid Zellipour
 # https://github.com/FaridZelli
+# Last updated 2024-8-22 4:39 AM
 
 # Check the current user
 USER=$(whoami)
@@ -15,13 +16,13 @@ else
   # Non-root user detected
   echo -e "
 --------------------------------------------------
-\033[33mWARNING: You do not seem to be logged in as root!\033[0m
+\033[31mWARNING: You do not seem to be logged in as root!\033[0m
 --------------------------------------------------"
 fi
 
 # Ask whether to proceed
 echo -e "
-This script will install and configure ROCm on your \033[32mopenSUSE Tumbleweed\033[0m system.
+This script will install and configure ROCm OpenCL 6.2 on your \033[32mopenSUSE Tumbleweed\033[0m system.
 I am not responsible for any damage or data loss that may occur.
 
 \033[33mDo you wish to continue? (Y/N/R)\033[0m
@@ -36,8 +37,9 @@ case $ANSWER in
   [Rr]* ) 
     # Proceed with the rest of the script
     echo "Removing ROCm..."
-    zypper remove -u rocm-opencl rocm-opencl-devel rocminfo rocm-smi-lib rocm-opencl-icd-loader clinfo
-    zypper removerepo ROCm
+    zypper remove -u rocm-opencl rocm-opencl-devel rocm-opencl-icd-loader rocm-smi-lib rocminfo
+    zypper removerepo rocm
+    zypper removerepo amdgpu
     echo -e "
 --------------------------------------------------
 \033[32mROCm has been removed from your system.\033[0m
@@ -51,19 +53,33 @@ case $ANSWER in
     ;;
 esac
 
+rm -f /etc/zypp/repos.d/rocm.repo
+rm -f /etc/zypp/repos.d/amdgpu.repo
+
 tee --append /etc/zypp/repos.d/rocm.repo <<EOF
-[ROCm-latest]
-name=ROCm
+[rocm]
+name=ROCm 6.2
+enabled=1
+autorefresh=1
+baseurl=https://repo.radeon.com/amdgpu/6.2/sle/15.6/main/x86_64/
+priority=50
+gpgcheck=1
+gpgkey=https://repo.radeon.com/rocm/rocm.gpg.key
+EOF
+
+tee --append /etc/zypp/repos.d/amdgpu.repo <<EOF
+[amdgpu]
+name=AMDGPU 6.2
 enabled=1
 autorefresh=1
 baseurl=https://repo.radeon.com/rocm/zyp/latest/main
-priority=90
+priority=50
 gpgcheck=1
 gpgkey=https://repo.radeon.com/rocm/rocm.gpg.key
 EOF
 
 zypper refresh
-zypper --gpg-auto-import-keys install rocm-opencl rocm-opencl-devel rocminfo rocm-smi-lib rocm-opencl-icd-loader clinfo
+zypper --gpg-auto-import-keys install rocm-opencl rocm-opencl-devel rocm-opencl-icd-loader rocm-smi-lib rocminfo clinfo
 
 # End of script
 echo -e "
